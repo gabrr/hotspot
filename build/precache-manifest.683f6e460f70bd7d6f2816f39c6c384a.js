c6c41453fdff4a1a2194b226067ad71ecad65db6
Gabriel Oliveira <g.webdevelopr@gmail.com>
2019-12-18 17:12:05 -0300

    build

---
+++ build/precache-manifest.683f6e460f70bd7d6f2816f39c6c384a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60135071f2063b44abfed12b9dff5a14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f883dabd0091ac873cde",
+    "url": "/static/css/main.43f6ca72.chunk.css"
+  },
+  {
+    "revision": "dc40846676d64a1273d5",
+    "url": "/static/js/2.bf20b758.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bf20b758.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f883dabd0091ac873cde",
+    "url": "/static/js/main.dd7fb248.chunk.js"
+  },
+  {
+    "revision": "b7e95d70b966a88fca63",
+    "url": "/static/js/runtime-main.ced525b7.js"
+  }
+]);
